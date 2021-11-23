aa89f5059c21717c5f35ef32459057b3b6a8a1d8
Jorge Galvis <jorlugaqui@gmail.com>
2021-11-22 23:19:36 -0500

    feat: Add initial (draft) sentiment GET endpoint

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-exports.getSentiments = (req, res) => {
-    res.send('Hello World!')
+async function analyzeSentimentsOfText(text) {
+    const language = require('@google-cloud/language');
+    const client = new language.LanguageServiceClient();
+    const document = {
+        content: text,
+        type: 'PLAIN_TEXT',
+    };
+    const [result] = await client.analyzeSentiment({document});
+    return result;
+}
+
+exports.getSentiments = async(req, res) => {
+    const result = await analyzeSentimentsOfText("this country sucks");
+    res.json(result);
 };
